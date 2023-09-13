5089d9419dd5ab5966e143aa578c76131bebef3b
pinomaker-hoo <inhoo23@naver.com>
2023-09-13 18:19:15 +0900

    fix : Exception Filter

--- src/exception/httpExceptionFilter.ts
+++ src/exception/httpExceptionFilter.ts
@@ -2,26 +2,49 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
+  Logger,
   HttpException,
-  HttpStatus,
 } from '@nestjs/common';
 
-@Catch(HttpException)
-export class AllExceptionFilter implements ExceptionFilter {
-  async catch(exception: HttpException, host: ArgumentsHost) {
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private logger = new Logger();
+
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
 
-    const status = exception
-      ? exception.getStatus()
-      : HttpStatus.INTERNAL_SERVER_ERROR;
+    const { method, originalUrl } = request;
+    const { name, message } = exception;
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: exception.message,
+    this.logger.error('===== ERROR =====');
+    this.logger.error(`${method} : ${originalUrl}`);
+
+    if (request.body) {
+      this.logger.error(`Request Body : ${JSON.stringify(request.body)}`);
+    }
+
+    if (request.query) {
+      this.logger.error(`Request Query : ${JSON.stringify(request.query)}`);
+    }
+
+    if (exception instanceof HttpException) {
+      this.logger.error(`[${exception.getStatus()}] ${message}`);
+
+      response.status(exception?.getStatus() || 400).json({
+        statusCode: exception?.getStatus() || 400,
+        error: name,
+        message,
+      });
+
+      return;
+    }
+
+    response.status(500).json({
+      statusCode: 500,
+      error: name,
+      message,
     });
   }
 }
