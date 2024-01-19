be2c43bae3e04085caf37737bf7e6eeb68331cfb
pinomaker-hoo <inhoo987654321@gmail.com>
2024-01-20 00:33:06 +0900

    feat : Add deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,21 @@
+if [ $# -eq 0 ]; then
+    echo "인자가 없습니다. 스크립트를 호출할 때 인자를 전달해주세요."
+    exit 1
+elif [ $1 = "core" ]; then
+    rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/dice/apps/core-server/
+    # ssh -t dice "sh ./dice/restart.sh"
+elif [ $1 = "auth" ]; then
+    rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/dice/apps/auth-server/
+    ssh -t dice "sh ./dice/restart.sh auth"
+elif [ $1 = "log" ]; then
+    rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/dice/apps/log-server/
+    ssh -t dice "sh ./dice/restart.sh log"
+elif [ $1 = "push" ]; then
+    rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/dice/apps/push-server/
+    ssh -t dice "sh ./dice/restart.sh push"
+else
+    echo "인자가 잘못되었습니다."
+    exit 1
+
+fi
+
