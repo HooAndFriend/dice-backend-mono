b60bc02fb9a53f9f84fa75cc8b6d54b8bcdaece4
pinomaker-hoo <inhoo987654321@gmail.com>
2024-01-23 21:33:51 +0900

    fix : deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,16 +3,16 @@ if [ $# -eq 0 ]; then
     exit 1
 elif [ $1 = "core" ]; then
     rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/dice/apps/core-server/
-    ssh -t dice "sh ./dice/restart.sh core"
+    ssh -t dice "cd ~/dice && sh restart.sh core"
 elif [ $1 = "auth" ]; then
     rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/dice/apps/auth-server/
-    ssh -t dice "sh ./dice/restart.sh auth"
+    ssh -t dice "cd ~/dice && sh restart.sh auth"
 elif [ $1 = "log" ]; then
     rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/dice/apps/log-server/
-    ssh -t dice "sh ./dice/restart.sh log"
+    ssh -t dice "cd ~/dice && sh restart.sh log"
 elif [ $1 = "push" ]; then
     rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/dice/apps/push-server/
-    ssh -t dice "sh ./dice/restart.sh push"
+    ssh -t dice "cd ~/dice && sh restart.sh push"
 else
     echo "인자가 잘못되었습니다."
     exit 1
