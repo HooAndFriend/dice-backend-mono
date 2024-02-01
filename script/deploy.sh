79a9abe034d21a45d860101e43deeb9a9a857324
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-01 09:24:19 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,29 +2,31 @@ if [ $# -eq 0 ]; then
     echo "인자가 없습니다. 스크립트를 호출할 때 인자를 전달해주세요."
     exit 1
 elif [ $1 = "core" ]; then
-    rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/dice/apps/core-server/
+    rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/apps/core-server/
     ssh -t dice "cd ~/dice && sh restart.sh core"
 elif [ $1 = "auth" ]; then
-    rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/dice/apps/auth-server/
+    rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/apps/auth-server/
     ssh -t dice "cd ~/dice && sh restart.sh auth"
 elif [ $1 = "log" ]; then
-    rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/dice/apps/log-server/
+    rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/apps/log-server/
     ssh -t dice "cd ~/dice && sh restart.sh log"
 elif [ $1 = "push" ]; then
-    rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/dice/apps/push-server/
+    rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/apps/push-server/
     ssh -t dice "cd ~/dice && sh restart.sh push"
 elif [ $1 = "admin" ]; then
-    rsync -av --exclude='node_modules' ./apps/admin-server/ dice:~/dice/apps/admin-server/
+    rsync -av --exclude='node_modules' ./apps/admin-server/ dice:~/apps/admin-server/
     ssh -t dice "cd ~/dice && sh restart.sh admin"
 elif [ $1 = "all" ]; then
-    rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/dice/apps/core-server/
-    ssh -t dice "cd ~/dice && sh restart.sh core"
-    rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/dice/apps/auth-server/
-    ssh -t dice "cd ~/dice && sh restart.sh auth"
-    rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/dice/apps/log-server/
-    ssh -t dice "cd ~/dice && sh restart.sh log"
-    rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/dice/apps/push-server/
-    ssh -t dice "cd ~/dice && sh restart.sh push"
+    rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/apps/core-server/
+    # ssh -t dice "cd ~/dice && sh restart.sh core"
+    rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/apps/auth-server/
+    # ssh -t dice "cd ~/dice && sh restart.sh auth"
+    rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/apps/log-server/
+    # ssh -t dice "cd ~/dice && sh restart.sh log"
+    rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/apps/push-server/
+    # ssh -t dice "cd ~/dice && sh restart.sh push"
+    rsync -av --exclude='node_modules' ./apps/admin-server/ dice:~/apps/admin-server/
+    # ssh -t dice "cd ~/dice && sh restart.sh admin"
 else
     echo "인자가 잘못되었습니다."
     exit 1
