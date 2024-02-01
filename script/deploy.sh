14574dcb0156b204ce18c4a2d24c4d1458069d20
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-01 09:24:49 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,30 +3,30 @@ if [ $# -eq 0 ]; then
     exit 1
 elif [ $1 = "core" ]; then
     rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/apps/core-server/
-    ssh -t dice "cd ~/dice && sh restart.sh core"
+    ssh -t dice "sh restart.sh core"
 elif [ $1 = "auth" ]; then
     rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/apps/auth-server/
-    ssh -t dice "cd ~/dice && sh restart.sh auth"
+    ssh -t dice "sh restart.sh auth"
 elif [ $1 = "log" ]; then
     rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/apps/log-server/
-    ssh -t dice "cd ~/dice && sh restart.sh log"
+    ssh -t dice "sh restart.sh log"
 elif [ $1 = "push" ]; then
     rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/apps/push-server/
-    ssh -t dice "cd ~/dice && sh restart.sh push"
+    ssh -t dice "sh restart.sh push"
 elif [ $1 = "admin" ]; then
     rsync -av --exclude='node_modules' ./apps/admin-server/ dice:~/apps/admin-server/
-    ssh -t dice "cd ~/dice && sh restart.sh admin"
+    ssh -t dice "sh restart.sh admin"
 elif [ $1 = "all" ]; then
     rsync -av --exclude='node_modules' ./apps/core-server/ dice:~/apps/core-server/
-    # ssh -t dice "cd ~/dice && sh restart.sh core"
+    ssh -t dice "sh restart.sh core"
     rsync -av --exclude='node_modules' ./apps/auth-server/ dice:~/apps/auth-server/
-    # ssh -t dice "cd ~/dice && sh restart.sh auth"
+    ssh -t dice "sh restart.sh auth"
     rsync -av --exclude='node_modules' ./apps/log-server/ dice:~/apps/log-server/
-    # ssh -t dice "cd ~/dice && sh restart.sh log"
+    ssh -t dice "sh restart.sh log"
     rsync -av --exclude='node_modules' ./apps/push-server/ dice:~/apps/push-server/
-    # ssh -t dice "cd ~/dice && sh restart.sh push"
+    ssh -t dice "sh restart.sh push"
     rsync -av --exclude='node_modules' ./apps/admin-server/ dice:~/apps/admin-server/
-    # ssh -t dice "cd ~/dice && sh restart.sh admin"
+    ssh -t dice "sh restart.sh admin"
 else
     echo "인자가 잘못되었습니다."
     exit 1
