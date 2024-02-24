1aa031f1ac68fcb5695a68ce340d88c0aa455f73
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-02-25 02:47:22 +0900

    feat : Add Find QA Admin API

--- apps/admin-server/src/modules/qa/repository/qa.repository.ts
+++ apps/admin-server/src/modules/qa/repository/qa.repository.ts
@@ -8,6 +8,58 @@ import CustomRepository from '../../../global/repository/typeorm-ex.decorator';
 import Qa from '../domain/qa.entity';
 // ** Emum Imports
 import { QaStatus } from '../../../global/enum/QaStatus.enum';
+import RequestQaFindDto from '../dto/qa.find.dto';
 
 @CustomRepository(Qa)
-export default class QaRepository extends Repository<Qa> {}
+export default class QaRepository extends Repository<Qa> {
+    public async findQaList(findQuery: RequestQaFindDto) {
+        const queryBuilder = this.createQueryBuilder('qa')
+            .select([
+                'qa.id',
+                'qa.number',
+                'qa.status',
+                'qa.title',
+                'qa.asIs',
+                'qa.toBe',
+                'qa.memo',
+                'qa.adminId',
+                'admin.email',
+                'admin.nickname',
+                'admin.profile',
+                'file.id',
+                'file.url',
+                'qa.createdDate',
+                'qa.modifiedDate',
+            ])
+            .leftJoin('qa.admin', 'admin')
+            .leftJoin('qa.file', 'file');
+
+        if (findQuery.status !== QaStatus.ALL) {
+            queryBuilder.andWhere('qa.status = :status', {
+                status: findQuery.status,
+            });
+        }
+        if (findQuery.title) {
+            queryBuilder.andWhere('qa.title LIKE :title', {
+                title: `%${findQuery.title}%`,
+            });
+        }
+        if (findQuery.number) {
+            queryBuilder.andWhere('qa.number LIKE :number', {
+                number: `%${findQuery.number}%`,
+            });
+        }
+        if (findQuery.createdDate ) {
+            queryBuilder.andWhere('qa.createdDate >= :createdDate', {
+                createdDate: `${findQuery.createdDate}`,
+            });
+        }
+        if (findQuery.modifiedDate ) {
+            queryBuilder.andWhere('qa.modifiedDate <= :modifiedDate', {
+                modifiedDate: `${findQuery.modifiedDate}`,
+            });
+        }
+
+        return await queryBuilder.getManyAndCount();
+    }
+}
