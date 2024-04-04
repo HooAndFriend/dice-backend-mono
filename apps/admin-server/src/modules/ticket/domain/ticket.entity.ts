c1fb818c146476e6e45a43ade27c3ed210f142b1
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-04 21:44:06 +0900

    Update ticket.entity.ts

--- apps/admin-server/src/modules/ticket/domain/ticket.entity.ts
+++ apps/admin-server/src/modules/ticket/domain/ticket.entity.ts
@@ -60,6 +60,7 @@ export default class Ticket extends BaseTimeEntity {
     type: 'integer',
     name: 'storypoint',
     comment: 'storypoint',
+    default: 0,
     nullable: true,
   })
   storypoint: number;
