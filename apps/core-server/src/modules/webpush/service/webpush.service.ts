8ba0208dbff5fe2bb1e48232b5f035dead5f95dd
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-04-15 00:56:06 +0900

    feat : fcm push messaging service

---
+++ apps/core-server/src/modules/webpush/service/webpush.service.ts
@@ -0,0 +1,45 @@
+// ** Nest Imports
+import { Injectable } from '@nestjs/common';
+import admin from 'firebase-admin';
+
+// ** Custom Module Imports
+import { BadRequestException } from '../../../global/exception/CustomException';
+// ** Response Imports
+
+// ** enum, dto, entity, types Imports
+
+var ServiceAccount = require('./fcm-account.json');
+
+@Injectable()
+export default class WebPushService {
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert(ServiceAccount),
+    });
+  }
+
+  /**
+   * Send push message
+   * @param token
+   * @param title
+   * @param body
+   */
+  public async sendPushMessage(token: string, title: string, body: string) {
+    //메시지 객체 생성
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      // 클라이언트 토큰
+      token: token,
+    };
+
+    try {
+      // push 메시지 전송
+      await admin.messaging().send(message);
+    } catch (error) {
+      throw new BadRequestException('Failed to send message');
+    }
+  }
+}
