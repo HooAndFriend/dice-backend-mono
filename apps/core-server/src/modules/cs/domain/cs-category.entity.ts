c921ac3353a0889e23d9b6f1ed27dcbe3715c0fd
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-22 00:42:53 +0900

    feat(core, admin) : Cs Category에 생성자와 수정자 컬럼을 추가 합니다.

--- apps/core-server/src/modules/cs/domain/cs-category.entity.ts
+++ apps/core-server/src/modules/cs/domain/cs-category.entity.ts
@@ -25,6 +25,22 @@ export default class CsCategory extends BaseTimeEntity {
   })
   name: string;
 
+  @Column({
+    type: 'varchar',
+    length: 120,
+    comment: '생성자 ID',
+    nullable: false,
+  })
+  createdId: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    comment: '수정자 ID',
+    nullable: false,
+  })
+  modifiedId: string;
+
   @OneToMany(() => Faq, (faq) => faq.csCategory)
   faq: Relation<Faq>[];
 
