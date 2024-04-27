e04e03951ab1758538c9de4e0dacfe8f22697924
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-04-28 02:01:40 +0900

    feat(core) : sprint 엔티티 추가합니다.

--- apps/core-server/src/modules/sprint/domain/sprint.entity.ts
+++ apps/core-server/src/modules/sprint/domain/sprint.entity.ts
@@ -1,15 +1,53 @@
 // ** Typeorm Imports
 import {
+    Column,
     Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
+    Relation,
   } from 'typeorm';
   
   // ** enum, dto, entity Imports
   import { BaseTimeEntity } from '@repo/common';
+import Ticket from '../../ticket/domain/ticket.entity';
   
   @Entity({ name: 'TB_SPRINT' })
   export default class Sprint extends BaseTimeEntity {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 30,
+        comment: '스프린트 이름',
+        nullable: false,
+    })
+    name: string;
+
+    @Column({
+        type: 'date',
+        name: 'start_date',
+        comment: '시작일',
+        nullable: false,
+    })
+    startDate: Date;
+
+    @Column({
+        type: 'date',
+        name: 'end_date',
+        comment: '마감일',
+        nullable: false,
+    })
+    endDate: Date;
+
+    @Column({
+        type: 'int',
+        comment: '정렬 순서',
+        nullable: false,
+    })
+    orderId: number;
+
+    @OneToMany(() => Ticket, (ticket) => ticket.id, { nullable: true })
+    ticket: Relation<Ticket>[];
   }
   
