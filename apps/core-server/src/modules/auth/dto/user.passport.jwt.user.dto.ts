ef03c248a8c296a987fa82d8e178e469e261b9a2
pinomaker-hoo <inhoo23@naver.com>
2024-04-28 01:02:55 +0900

    del(core) : 미사용 dto를 삭제합니다.

--- apps/core-server/src/modules/auth/dto/user.passport.jwt.user.dto.ts
+++
@@ -1,6 +0,0 @@
-// ** enum, dto, entity Imports
-import User from '../../user/domain/user.entity';
-
-export default interface RequestPassportJwtDto extends Request {
-  user: User;
-}
--- apps/core-server/src/modules/auth/dto/user.passport.user.dto.ts
+++
@@ -1,6 +0,0 @@
-// ** enum, dto, entity Imports
-import ApiResponse from '../../../global/dto/api.response';
-
-export default interface RequestPassportDto extends Request {
-  user: ApiResponse<any>;
-}
