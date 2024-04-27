42abb9398440b6f1c3de7abfd10db905d3dfa624
pinomaker-hoo <inhoo23@naver.com>
2024-04-28 00:15:22 +0900

    feat(admin, log) : Base Domain을 제거 합니다.

--- apps/log-server/src/global/domain/BaseCreatedTime.entity.ts
+++
@@ -1,7 +0,0 @@
-// ** Typeorm Imports
-import { BaseEntity, CreateDateColumn } from 'typeorm';
-
-export default abstract class BaseCreatedTimeEntity extends BaseEntity {
-  @CreateDateColumn({ name: 'created_date' })
-  createdDate: Date;
-}
