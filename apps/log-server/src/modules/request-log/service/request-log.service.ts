6f872fe2771451ad517ec114fc3b425fbdd040b8
pinomaker-hoo <inhoo23@naver.com>
2024-04-27 22:12:01 +0900

    fix(log) : Request Log 저장 시에 IP 예외 처리

--- apps/log-server/src/modules/request-log/service/request-log.service.ts
+++ apps/log-server/src/modules/request-log/service/request-log.service.ts
@@ -28,7 +28,7 @@ export default class RequestLogService {
     requestLog.requestUrl = dto.requestUrl;
     requestLog.requestBody = JSON.stringify(dto.requestBody);
     requestLog.responseBody = JSON.stringify(dto.responseBody);
-    requestLog.ip = dto.ip;
+    requestLog.ip = dto.ip ? dto.ip : '';
     requestLog.userId = dto.userId;
 
     return requestLog;
