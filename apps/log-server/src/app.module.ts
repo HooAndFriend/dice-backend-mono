497a1a1c13f822d40b057607e6c6478cb47e0c5a
pinomaker-hoo <inhoo23@naver.com>
2024-04-30 16:38:08 +0900

    feat(log) : Admin Server에 typeorm-transactional을 추가합니다.

--- apps/log-server/src/app.module.ts
+++ apps/log-server/src/app.module.ts
@@ -6,6 +6,8 @@ import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 // ** Typeorm Imports
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 // ** Custom Module Imports
 import { TypeOrmExModule } from './global/repository/typeorm-ex.module';
@@ -21,31 +23,40 @@ import { LoggingInterceptor } from './global/interceptor/LoggingInterceptor';
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      entities: ['dist/modules/**/*.entity.js'],
-      synchronize: true,
-      logging: true,
-      logger: 'file',
-      charset: 'utf8mb4_unicode_ci',
-      timezone: '+09:00',
-      replication: {
-        master: {
-          host: process.env.DB_MASTER_HOST,
-          port: +process.env.DB_MASTER_PORT,
-          username: process.env.DB_MASTER_USERNAME,
-          password: process.env.DB_MASTER_PASSWORD,
-          database: process.env.DB_MASTER_DATABASE,
-        },
-        slaves: [
-          {
-            host: process.env.DB_MASTER_HOST,
-            port: +process.env.DB_MASTER_PORT,
-            username: process.env.DB_MASTER_USERNAME,
-            password: process.env.DB_MASTER_PASSWORD,
-            database: process.env.DB_MASTER_DATABASE,
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return {
+          type: 'mysql',
+          entities: ['dist/modules/**/*.entity.js'],
+          synchronize: true,
+          logging: true,
+          logger: 'file',
+          charset: 'utf8mb4_unicode_ci',
+          timezone: '+09:00',
+          replication: {
+            master: {
+              host: process.env.DB_MASTER_HOST,
+              port: +process.env.DB_MASTER_PORT,
+              username: process.env.DB_MASTER_USERNAME,
+              password: process.env.DB_MASTER_PASSWORD,
+              database: process.env.DB_MASTER_DATABASE,
+            },
+            slaves: [
+              {
+                host: process.env.DB_MASTER_HOST,
+                port: +process.env.DB_MASTER_PORT,
+                username: process.env.DB_MASTER_USERNAME,
+                password: process.env.DB_MASTER_PASSWORD,
+                database: process.env.DB_MASTER_DATABASE,
+              },
+            ],
           },
-        ],
+        };
+      },
+      async dataSourceFactory(option) {
+        if (!option) throw new Error('Invalid options passed');
+
+        return addTransactionalDataSource(new DataSource(option));
       },
     }),
     RedisModule.forRoot({
