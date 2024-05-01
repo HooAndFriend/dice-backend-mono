6a19a1aeec56bba3b76d6666084be816391a0f9d
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-05-02 02:54:37 +0900

    feat(core) : sprint service에 logger 추가합니다.

--- apps/core-server/src/modules/sprint/service/sprint.service.ts
+++ apps/core-server/src/modules/sprint/service/sprint.service.ts
@@ -1,5 +1,5 @@
 // ** Nest Imports
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 
 // ** Custom Module Imports
 import { In } from 'typeorm';
@@ -9,6 +9,7 @@ import TicketRepository from '../../ticket/repository/ticket.repository';
 // ** enum, dto, entity, types Imports
 import RequestSprintSaveDto from '../dto/sprint.save.dto';
 import RequestSprintUpdateDto from '../dto/sprint.update.dto';
+import { InternalServerErrorException } from '@repo/common';
 
 @Injectable()
 export default class SprintService {
@@ -16,19 +17,30 @@ export default class SprintService {
     private readonly sprintRepository: SprintRepository,
     private readonly ticketRepository: TicketRepository,
   ) {}
+  
+  private logger = new Logger(SprintService.name);
 
   public async saveSprint(dto: RequestSprintSaveDto) {
     const findTickets = await this.ticketRepository.findBy({ id: In(dto.ticketIds) });
+    try{
+      await this.sprintRepository.save(
+        {
+          name: dto.sprintName,
+          startDate: dto.startDate,
+          endDate: dto.endDate,
+          orderId: dto.orderId,
+          ticket: findTickets,
+        }
+      );
+    } catch (error) {
+      this.logger.log(error);
 
-    await this.sprintRepository.save(
-      {
-        name: dto.sprintName,
-        startDate: dto.startDate,
-        endDate: dto.endDate,
-        orderId: dto.orderId,
-        ticket: findTickets,
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
       }
-    );
+
+      throw new InternalServerErrorException('Internal Server Error');
+    }
   }
 
   public async findSprint(sprintId: number) {
@@ -47,10 +59,19 @@ export default class SprintService {
 
   public async updateSprint(dto: RequestSprintUpdateDto) {
     const findSprint = await this.findSprint(dto.sprintId);
+    try{
+      const findTickets = await this.ticketRepository.findBy({ id : In(dto.ticketIds)});
 
-    const findTickets = await this.ticketRepository.findBy({ id : In(dto.ticketIds)});
+      findSprint.updateSprintFromDto(dto, findTickets);
+    } catch (error) {
+      this.logger.log(error);
 
-    findSprint.updateSprintFromDto(dto, findTickets);
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      }
+
+      throw new InternalServerErrorException('Internal Server Error');
+    }
   }
 
   public async deleteSprint(sprintId: number) {
