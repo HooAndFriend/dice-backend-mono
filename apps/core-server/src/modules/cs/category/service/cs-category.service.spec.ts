85e9ad235e65c4ae6091faed2ab4962e01eeb2d0
pinomaker-hoo <inhoo23@naver.com>
2024-05-08 00:41:58 +0900

    feat(core) : cscategory Service Test 코드를 작성합니다.

---
+++ apps/core-server/src/modules/cs/category/service/cs-category.service.spec.ts
@@ -0,0 +1,66 @@
+// ** Nest Imports
+import { Test } from '@nestjs/testing';
+
+// ** Module Imports
+import CsCategoryService from './cs-category.service';
+import CsCategoryRepository from '../repository/cs-category.repository';
+
+// ** Entity Imports
+import { NotFoundException } from '@hi-dice/common';
+import CsCategory from '../domain/cs-category.entity';
+
+describe('CsCategory Service Test', () => {
+  let service;
+  let repository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [CsCategoryService, CsCategoryRepository],
+    }).compile();
+
+    service = module.get(CsCategoryService);
+    repository = module.get(CsCategoryRepository);
+  });
+
+  describe('Find CsCategory By Id Test', () => {
+    it('CS Category가 없습니다.', () => {
+      // ** Given
+      jest.spyOn(repository, 'findOne').mockReturnValue(null);
+
+      // ** When
+      const response = service.findCsCategoryById();
+
+      // ** Then
+      expect(response).rejects.toThrowError(
+        new NotFoundException('Not Found CsCategory'),
+      );
+    });
+
+    it('CS Category가 있을 경우 CS Category를 반환합니다.', () => {
+      // ** Given
+      const csCategory = getCsCategory();
+      jest.spyOn(repository, 'findOne').mockReturnValue(csCategory);
+
+      // ** When
+      const response = service.findCsCategoryById();
+
+      // ** Then
+      expect(response);
+    });
+  });
+});
+
+/**
+ * Get CsCategory Dummy
+ * @returns
+ */
+export const getCsCategory = () => {
+  const csCategory = new CsCategory();
+  csCategory.name = 'test';
+  csCategory.createdId = 'test';
+  csCategory.modifiedId = 'test';
+  csCategory.createdDate = new Date();
+  csCategory.modifiedDate = new Date();
+
+  return csCategory;
+};
--- apps/core-server/src/modules/cs/category/service/cs-category.service.ts
+++ apps/core-server/src/modules/cs/category/service/cs-category.service.ts
@@ -1,6 +1,5 @@
 // ** Nest Imports
 import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 // ** Custom Module Imports
 import CsCategoryRepository from '../repository/cs-category.repository';
@@ -10,10 +9,7 @@ import { NotFoundException } from '@hi-dice/common';
 
 @Injectable()
 export default class CsCategoryService {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly csCategoryRepository: CsCategoryRepository,
-  ) {}
+  constructor(private readonly csCategoryRepository: CsCategoryRepository) {}
 
   private logger = new Logger(CsCategoryService.name);
 
