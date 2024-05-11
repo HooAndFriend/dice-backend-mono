7a5ebf68981e220fe2e9e3041a2e4ebbc6733a1c
pinomaker-hoo <inhoo23@naver.com>
2024-05-12 00:34:52 +0900

    fix(log) : Internal Core Sender 시에 로그 추가.

--- apps/log-server/src/modules/internal/internal-core/service/internal-core.sender.service.ts
+++ apps/log-server/src/modules/internal/internal-core/service/internal-core.sender.service.ts
@@ -1,5 +1,5 @@
 // ** Nest Imports
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 // ** Utils Imports
@@ -11,6 +11,7 @@ import { InternalResponse, UserProfile } from '../types';
 @Injectable()
 export default class InternalCoreSenderService {
   private coreRepository: AxiosInstance;
+  private logger = new Logger(InternalCoreSenderService.name);
 
   constructor(private readonly configService: ConfigService) {
     this.coreRepository = axios.create({
@@ -29,11 +30,15 @@ export default class InternalCoreSenderService {
    * @returns
    */
   public async findUserProfileList(emailList: string[]) {
+    this.logger.log(`findUserProfileList : ${JSON.stringify(emailList)}`);
+
     const { data } = await this.coreRepository.post<
       InternalResponse<UserProfile[]>
     >('/internal/log', {
       email: emailList,
     });
+
+    this.logger.log(`findUserProfileList Response : ${JSON.stringify(data)}`);
 
     return data;
   }
