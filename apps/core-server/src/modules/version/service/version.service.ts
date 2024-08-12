a53e086582218235c1695bd35702761593339953
pinomaker-hoo <inhoo23@naver.com>
2024-08-12 16:38:33 +0900

    ref(core) : Version Service의 메서드

--- apps/core-server/src/modules/version/service/version.service.ts
+++ apps/core-server/src/modules/version/service/version.service.ts
@@ -4,23 +4,20 @@ import { ConfigService } from '@nestjs/config';
 
 // ** Module Imports
 import VersionRepository from '../repository/version.repository';
-import VersionTypeEnum from '../domain/version-type.enum';
-import { BadRequestException } from '@/src/global/exception/CustomException';
-
-// ** Utils Imports
 
 // ** enum, dto, entity, types Imports
+import VersionTypeEnum from '../domain/version-type.enum';
+import { BadRequestException } from '@/src/global/exception/CustomException';
+import Version from '../domain/version.entity';
 
 @Injectable()
 export default class VersionService {
   constructor(private readonly versionRepository: VersionRepository) {}
 
   /**
-   * Find Lastest Version
-   * @param type
-   * @returns
+   * 해당 Type의 최신 버전을 조회합니다.
    */
-  public async findLastestVesrion(type: VersionTypeEnum) {
+  public async findLastestVesrion(type: VersionTypeEnum): Promise<Version> {
     const latestVersion = await this.versionRepository.findOne({
       where: { type },
       order: { version: 'DESC' },
