4d6b5ebc3c5ef5ba07debabd2bb979a6faff6c63
pinomaker-hoo <inhoo23@naver.com>
2024-08-21 15:29:04 +0900

    feat(common) : Qa History Log Dto 제거

--- packages/common/libs/dto/log/qa-history-log.save.dto.ts
+++
@@ -1,8 +0,0 @@
-import { QaHistoryTypeEnum } from '../../enum/log/QaHistoryLogType.enum';
-
-export class RequestQaHistoryLogSaveDto {
-  qaId: number;
-  email: string;
-  type: QaHistoryTypeEnum;
-  log: string;
-}
