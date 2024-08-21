b17510713946ececd6e146c6e66d480fe8f9d00b
pinomaker-hoo <inhoo23@naver.com>
2024-08-21 15:38:35 +0900

    feat(common) : TicketHistoryLogDto에 Optional 추가

--- packages/common/libs/dto/log/ticket-history-log.save.dto.ts
+++ packages/common/libs/dto/log/ticket-history-log.save.dto.ts
@@ -1,17 +1,17 @@
 import { TicketHistoryTypeEnum } from '../../enum/log/TicketHistoryLogType.enum';
 
 export class RequestTicketHistoryLogSaveDto {
-  ticketId: number;
+  ticketId?: number;
   creatorEmail?: string;
   creatorNickname?: string;
-  creatorProfile: string;
-  type: TicketHistoryTypeEnum;
-  beforeLog: string;
-  beforeEmail: string;
-  beforeNickname: string;
-  beforeProfile: string;
-  afterLog: string;
-  afterEmail: string;
-  afterNickname: string;
-  afterProfile: string;
+  creatorProfile?: string;
+  type?: TicketHistoryTypeEnum;
+  beforeLog?: string;
+  beforeEmail?: string;
+  beforeNickname?: string;
+  beforeProfile?: string;
+  afterLog?: string;
+  afterEmail?: string;
+  afterNickname?: string;
+  afterProfile?: string;
 }
