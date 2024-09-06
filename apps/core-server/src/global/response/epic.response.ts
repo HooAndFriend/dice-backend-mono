5653e3325226014b935bf432e9f2527f80abd480
pinomaker-hoo <inhoo23@naver.com>
2024-09-06 16:41:46 +0900

    feat(core) : Epic 조회 변경

--- apps/core-server/src/global/response/epic.response.ts
+++ apps/core-server/src/global/response/epic.response.ts
@@ -92,15 +92,137 @@ export const EpicResponse = {
     200: createResponse({
       data: {
         epicId: 1,
-        name: 'DICE 로그인 수정',
+        name: 'Task - Gantt',
         code: 'HAF-1',
-        content: 'DICE ',
+        content: '123',
         ticket: [
           {
-            ticketId: 4,
-            name: '게시판',
+            createdDate: '2024-08-12T01:11:09.644Z',
+            modifiedDate: '2024-08-21T12:13:06.000Z',
+            ticketId: 26,
+            name: '간트차트의 Progress바의 범위 벗어나는 버그 수정',
+            status: 'COMPLETE',
+            content: null,
+            code: 'HAF-8',
+            storypoint: 0,
+            dueDate: '2024-08-16',
+            completeDate: '2024-08-16',
+            reopenDate: null,
+            admin: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            worker: {
+              userId: 10,
+              email: 'naayeah1@gmail.com',
+              nickname: '백예나',
+              profile:
+                'https://file.hi-dice.com/file//20240818192412-áá¡á¼áá³áº áá®áá© áá¡á¯.jpeg',
+            },
+            ticketSetting: {
+              ticketSettingId: 2,
+              type: 'GREEN',
+              name: 'SCN',
+            },
+          },
+          {
+            createdDate: '2024-08-18T12:27:07.294Z',
+            modifiedDate: '2024-09-03T11:28:50.000Z',
+            ticketId: 60,
+            name: 'API 호출하여 리스트업하기',
             status: 'NOTHING',
-            code: 'HAF-1',
+            content: null,
+            code: 'HAF-40',
+            storypoint: 0,
+            dueDate: '2024-09-08',
+            completeDate: null,
+            reopenDate: null,
+            admin: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            worker: {
+              userId: 10,
+              email: 'naayeah1@gmail.com',
+              nickname: '백예나',
+              profile:
+                'https://file.hi-dice.com/file//20240818192412-áá¡á¼áá³áº áá®áá© áá¡á¯.jpeg',
+            },
+            ticketSetting: {
+              ticketSettingId: 4,
+              type: 'YELLOW',
+              name: 'FUNC',
+            },
+          },
+          {
+            createdDate: '2024-09-03T15:55:25.392Z',
+            modifiedDate: '2024-09-03T15:55:35.000Z',
+            ticketId: 104,
+            name: '필터 UI 구성',
+            status: 'NOTHING',
+            content: null,
+            code: 'HAF-80',
+            storypoint: 0,
+            dueDate: null,
+            completeDate: null,
+            reopenDate: null,
+            admin: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            worker: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            ticketSetting: {
+              ticketSettingId: 2,
+              type: 'GREEN',
+              name: 'SCN',
+            },
+          },
+          {
+            createdDate: '2024-09-03T15:55:30.421Z',
+            modifiedDate: '2024-09-03T15:55:33.000Z',
+            ticketId: 105,
+            name: '필터 구성',
+            status: 'NOTHING',
+            content: null,
+            code: 'HAF-81',
+            storypoint: 0,
+            dueDate: null,
+            completeDate: null,
+            reopenDate: null,
+            admin: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            worker: {
+              userId: 4,
+              email: 'inhoo987654321@gmail.com',
+              nickname: '김인후',
+              profile:
+                'https://file.hi-dice.com/file//20240813111450-314631462_1719969028389444_6312286304904119979_n.jpg',
+            },
+            ticketSetting: {
+              ticketSettingId: 4,
+              type: 'YELLOW',
+              name: 'FUNC',
+            },
           },
         ],
       },
