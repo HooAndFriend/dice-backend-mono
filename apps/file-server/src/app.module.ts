7991cc8c78c751b408033418a593d7a8e1adee95
pinomaker-hoo <inhoo23@naver.com>
2024-09-11 11:18:15 +0900

    feat(file) : Logging 변경

--- apps/file-server/src/app.module.ts
+++ apps/file-server/src/app.module.ts
@@ -11,6 +11,9 @@ import { CustomExceptionFilter } from './global/filter/CustomExceptionFilter';
 import { LoggingInterceptor } from './global/interceptor/LoggingInterceptor';
 import FileModule from '@/src/modules/file.module';
 
+// ** Utils Imports
+import { v4 as uuidv4 } from 'uuid';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -24,6 +27,14 @@ import FileModule from '@/src/modules/file.module';
     //     port: +process.env.REDIS_PORT,
     //   },
     // }),
+    ClsModule.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+        generateId: true,
+        idGenerator: (req: Request) => req.headers['X-Request-Id'] ?? uuidv4(),
+      },
+    }),
     ClientsModule.registerAsync([
       {
         name: 'RMQ_LOG_QUE',
