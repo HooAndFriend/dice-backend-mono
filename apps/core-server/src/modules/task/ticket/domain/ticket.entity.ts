a8b213d9404487fd61dde536ef761b099de41be1
yoona <103414935+yoonali@users.noreply.github.com>
2025-01-20 11:57:09 +0900

    feat(core) : Add columns in ticket entity
    - epic_order_id
    - sprint_order_id

--- apps/core-server/src/modules/task/ticket/domain/ticket.entity.ts
+++ apps/core-server/src/modules/task/ticket/domain/ticket.entity.ts
@@ -45,6 +45,22 @@ export default class Ticket extends BaseTimeEntity {
   orderId: number;
 
   @Column({
+    type: 'int',
+    comment: 'epic 내부 티켓 정렬 순서',
+    nullable: false,
+    default: 1,
+  })
+  epicOrderId: number;
+
+  @Column({
+    type: 'int',
+    comment: 'sprint 내부 티켓 정렬 순서',
+    nullable: false,
+    default: 1,
+  })
+  sprintOrderId: number;
+
+  @Column({
     type: 'enum',
     enum: PriorityEnum,
     default: PriorityEnum.MEDIUM,
