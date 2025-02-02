418af060774d52ac567cb68b39015b4b91449e00
pinomaker-hoo <inhoo23@naver.com>
2025-02-03 00:34:59 +0900

    feat(global) : 빌드 설정 변경

--- script/connect.sh
+++ script/connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -L 3307:localhost:3307 -L 3308:localhost:3308 -L 5672:localhost:5672 -L 6379:localhost:6379  -L 6380:localhost:6380 dice
+ssh -L 3306:localhost:3306 -L 5672:localhost:5672 -L 6379:localhost:6379  -L 6380:localhost:6380 dice
